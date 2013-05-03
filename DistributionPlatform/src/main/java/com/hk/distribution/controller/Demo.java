bebdfe9adb2a47369f6319a3400b1dc13f1659e0
JonWangM <88415931wm@163.com>
2013-05-03 11:45:57 +0800

    test submission of demo

---
+++ DistributionPlatform/src/main/java/com/hk/distribution/controller/Demo.java
@@ -0,0 +1,13 @@
+package com.hk.distribution.controller;
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
